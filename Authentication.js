29fc7c2c72008ce0c181eb8769ae974753fc4cda
Bryant Fussell <bryant.fussell@gmail.com>
2024-03-22 16:54:41 -0500

    added files

--- Authentication.js
+++ Authentication.js
@@ -1,74 +1,79 @@
 const express = require('express');
 const bodyParser=require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 var fs = require("fs");
 const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://Usernew:KhvAOKrTpSFCxWge@cluster0.atsm2tk.mongodb.net/?retryWrites=true&w=majority";
-
-
-
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
 // Middleware to handle jsons and URL encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-//WILL CHECK FOR COOKIE IN FUTURE NOT NOW!!!!!
 app.get('/', function(req, res, next){
-  res.send("You did not send me anything");
+ if (req.cookies.myCookie) {
+  return res.redirect('/result');
+ } 
+ else {
+  return res.redirect('/Form');
+ }
+});
+
+
+app.post('/login', async (req, res, next ) => {
+ const { username, password } = req.body;
+ try {
+  const client = await MongoClient.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ });
+
+ const db = client.db("MyDBexample");
+ const user = await db.collection('Users').findOne({ username, password });
+ //console.log('user:', user);
+
+ if (user) {
+  const expiryDate = new Date(Date.now() + 45 * 1000);
+  res.cookie('myCookie', 'Ice-Cream', { expires: expiryDate });
+  res.send("Ready To Roll")
+  next();
+ } 
+ else {
+  res.send('Invalid username or password');
+ }
+
+ client.close();
+ } 
+ catch (err) {
+  console.error(err);
+  res.status(500).send('Internal Server Error');
+ }
+});
+
+app.get('/login-success', (req, res) => {
+  res.redirect('/result');
 });
 
 
 
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+app.get("/result",function(req,res) {
+ fs.readFile('result.html','utf8',(err,data)=>{
+ if(err) {
+  res.send('some err occured ',err);
+ }
+ res.send(data);
+ })
+})
 
-  try {
-    const client = await MongoClient.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  
-    const db = client.db("MyDBexample");
-    const user = await db.collection('Users').findOne({ username, password });
-    console.log('user:', user);
-
-    if (user) {
-      res.send('Login successful');
-    } else {
-      res.send('Invalid username or password');
-    }
-
-    client.close();
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
-
-/*
-//  How to grab data from a POST request
-app.post('/post/users', function(req, res) {
-  const formBody= req.body;
-  var outstring='';
-  for(var key in formBody) { outstring += "--" + key + ">" + formBody.key; }
-
-  res.send('The formBody is: ' + JSON.stringify(formBody) + '<br>The outstring is: ' + outstring);
-});
-*/
-
-
-// Using a local file to generate a web form (like post.html)
-app.get("/getfile",function(req,res) {
-  fs.readFile('Form.html','utf8',(err,data)=>{
-    console.log(data)
-    if(err){
-      res.send('some err occured ',err);
-    }
-    res.send(data);
-  })
+app.get("/Form",function(req,res) {
+ fs.readFile('Form.html','utf8',(err,data)=>{
+ if(err) {
+  res.send('some err occured ',err);
+ }
+ res.send(data);
+ })
 })
